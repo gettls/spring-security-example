63db4d10dec10ea6f69f2470dcf51082174faa5d
gettls <gettls@daum.net>
2023-01-01 15:58:13 +0900

    2

--- src/main/java/example/security/config/SecurityConfig.java
+++ src/main/java/example/security/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package example.security.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.authorization.AuthorizationDecision;
 import org.springframework.security.authorization.AuthorizationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -13,11 +14,16 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
+import org.springframework.security.web.savedrequest.RequestCache;
+import org.springframework.security.web.savedrequest.SavedRequest;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -60,6 +66,7 @@ public class SecurityConfig {
 
 		http
 				.authorizeRequests()
+				.antMatchers("/login").permitAll()
 				.antMatchers("/user").hasRole("USER")
 				.antMatchers("/admin/pay").hasRole("ADMIN")
 				.antMatchers("/admin/**").access("hasRole('ADMIN') or hasRole('SYS')")
@@ -77,7 +84,16 @@ public class SecurityConfig {
 					@Override
 					public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
 						System.out.println("authentication" + authentication.getName());
-						response.sendRedirect("/");
+
+						// requestCache 
+						// -> 인증의 과정을 거치지 않은 사용자가 인증을 거친 사용자에게만 허용되는 자원에 접근할 때 생성됨
+						// -> 따라서, 바로 /login 으로 접근하면 savedRequest 는 null
+
+						RequestCache requestCache = new HttpSessionRequestCache();
+						SavedRequest savedRequest = requestCache.getRequest(request, response);
+						String redirectUrl = savedRequest.getRedirectUrl();
+
+						response.sendRedirect(redirectUrl);
 					}
 				})
 				.failureHandler(new AuthenticationFailureHandler() {
@@ -120,6 +136,27 @@ public class SecurityConfig {
 				.maxSessionsPreventsLogin(true)
 				.and()
 				.sessionFixation().changeSessionId();
+
+		http
+				.exceptionHandling()
+				.authenticationEntryPoint(new AuthenticationEntryPoint() {// 인증 예외
+					@Override
+					public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+						response.sendRedirect("/login");
+					}
+				})
+				.accessDeniedHandler(new AccessDeniedHandler() { // 인가 예외
+					@Override
+					public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
+						response.sendRedirect("/denied");
+					}
+				});
+
+		// CsrfFilter
+		// 모든 요청에 랜덤하게 생성된 토큰을 HTTP 파라미터로 요구 -> HTTP Header에 X-CSRF-TOKEN 있어야 함
+		// 전달되는 토큰 값과 서버에 저장된 실제 값을 비교, 불일치 시 요청 실패 처리
+		http
+				.csrf().disable();
 
 		return http.build();
 	}
